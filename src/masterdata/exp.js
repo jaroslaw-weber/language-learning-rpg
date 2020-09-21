1bd6b3a7b5b1d9d3b77c4b0c1c2cebeba4f0bb2e
Weber Jaroslaw <weber-jaroslaw@dmm.com>
2020-09-21 18:48:55 +0900

    added new location, fixed small bugs, refactor masterdata

---
+++ src/masterdata/exp.js
@@ -0,0 +1,127 @@
+//how much exp needed for each level
+export default [
+  {
+    level: 1,
+    exp: 0,
+  },
+  {
+    level: 2,
+    exp: 15,
+  },
+  {
+    level: 3,
+    exp: 30,
+  },
+  {
+    level: 4,
+    exp: 60,
+  },
+  {
+    level: 5,
+    exp: 100,
+  },
+  {
+    level: 6,
+    exp: 130,
+  },
+  {
+    level: 7,
+    exp: 170,
+  },
+  {
+    level: 8,
+    exp: 210,
+  },
+  {
+    level: 9,
+    exp: 240,
+  },
+  {
+    level: 10,
+    exp: 300,
+  },
+  {
+    level: 11,
+    exp: 350,
+  },
+  {
+    level: 12,
+    exp: 400,
+  },
+  {
+    level: 13,
+    exp: 460,
+  },
+  {
+    level: 14,
+    exp: 520,
+  },
+  {
+    level: 15,
+    exp: 580,
+  },
+  {
+    level: 16,
+    exp: 670,
+  },
+  {
+    level: 17,
+    exp: 790,
+  },
+  {
+    level: 18,
+    exp: 890,
+  },
+  {
+    level: 19,
+    exp: 1000,
+  },
+  {
+    level: 20,
+    exp: 1110,
+  },
+  {
+    level: 21,
+    exp: 1230,
+  },
+  {
+    level: 22,
+    exp: 1330,
+  },
+  {
+    level: 23,
+    exp: 1430,
+  },
+  {
+    level: 24,
+    exp: 1550,
+  },
+  {
+    level: 25,
+    exp: 1680,
+  },
+  {
+    level: 26,
+    exp: 1790,
+  },
+  {
+    level: 27,
+    exp: 1900,
+  },
+  {
+    level: 28,
+    exp: 2100,
+  },
+  {
+    level: 29,
+    exp: 2300,
+  },
+  {
+    level: 30,
+    exp: 2500,
+  },
+  {
+    level: 31,
+    exp: 2700,
+  },
+];
